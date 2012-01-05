b064c2f48449b7d8e3511f1fbdb573e457bca74a
Miaou <miaou.pbl@gmail.com>
2012-01-05 11:14:14 +0100

    Nouveau fichier, carte heuristique Idees

---
+++ Idees.mm
@@ -0,0 +1,53 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="ZombCraft" ID="ID_1420097720" CREATED="1325606038189" MODIFIED="1325606058653">
+<hook NAME="MapStyle" max_node_width="600"/>
+<node TEXT="Le jeu de Survival &#xe0; la sauce zombie et Minecraft" POSITION="right" ID="ID_1668612262" CREATED="1325606061246" MODIFIED="1325606079386"/>
+<node TEXT="Id&#xe9;es fondatrices" POSITION="right" ID="ID_229379485" CREATED="1325627807698" MODIFIED="1325627816033">
+<node TEXT="des zz" ID="ID_1741644693" CREATED="1325627816586" MODIFIED="1325627819214"/>
+<node TEXT="des vagues &#xe0; la hordes" ID="ID_993307353" CREATED="1325627819666" MODIFIED="1325627824151"/>
+<node TEXT="des constructions &#xe0; la minecraft" ID="ID_1961128701" CREATED="1325627824386" MODIFIED="1325627834030"/>
+<node TEXT="du RPG simple &#xe0; la Borderlands (voire plus simple)" ID="ID_1102700480" CREATED="1325628250441" MODIFIED="1325628285031"/>
+<node TEXT="(du level de zz &#xe0; la Borderlands)" ID="ID_83223473" CREATED="1325628257210" MODIFIED="1325628270461"/>
+<node TEXT="du moteur physique pour g&#xe9;rer les collisions, les armes &#xe0; feu, les armes autres, les dommages" ID="ID_1684519105" CREATED="1325629121050" MODIFIED="1325630007455"/>
+</node>
+<node TEXT="Id&#xe9;es d&#xe9;j&#xe0; en discussion" POSITION="right" ID="ID_1961856716" CREATED="1325628223658" MODIFIED="1325628233167">
+<node TEXT="2D vs 3D" ID="ID_722806428" CREATED="1325628234369" MODIFIED="1325628241883"/>
+<node TEXT="en 2D : 2D en vue en coupe (en haut le ciel, en bas la terre), ou 2D type 3D projet&#xe9;e comme dans les &quot;vieux&quot; RTS (en haut le nord, en bas le sud, impossible de voir dans des hypoth&#xe9;tiques grottes)" ID="ID_1727250835" CREATED="1325630035785" MODIFIED="1325630101303"/>
+<node TEXT="en 2D : creuser : hum hum :/" ID="ID_1643911507" CREATED="1325630618746" MODIFIED="1325630632558"/>
+<node TEXT="niveau de libert&#xe9; de crafting" ID="ID_955503498" CREATED="1325629969314" MODIFIED="1325629981583"/>
+<node TEXT="quels types de zz ?" ID="ID_1597154977" CREATED="1325630021314" MODIFIED="1325630029183"/>
+</node>
+<node TEXT="niveau de &quot;bac &#xe0; sable&quot;" POSITION="right" ID="ID_1887505115" CREATED="1325628242325" MODIFIED="1325629419641">
+<node TEXT="j&apos;avais pens&#xe9; au d&#xe9;but &#xe0; faire un truc le plus libre possible, qui laisse le plus de cr&#xe9;ation possible" ID="ID_968600621" CREATED="1325629138034" MODIFIED="1325629168519"/>
+<node TEXT="mais &#xe7;a reviendrait &#xe0; mod&#xe9;liser la chimie, la physique et autres..." ID="ID_1930411698" CREATED="1325629168794" MODIFIED="1325629183271"/>
+<node TEXT="donc il faudra cr&#xe9;er un cadre pour le craft..." ID="ID_957960356" CREATED="1325629183554" MODIFIED="1325629205279"/>
+<node TEXT="je voulais &#xe9;viter le plus possible qu&apos;il y ait des recettes toutes pr&#xea;tes comme dans MInecraft, et que le challenge soit de les d&#xe9;couvrir (car nous on les conna&#xee;tra vu qu&apos;on les aura invent&#xe9;es)" ID="ID_537176593" CREATED="1325629205690" MODIFIED="1325629250279"/>
+<node TEXT="mais j&apos;ai pas trop d&apos;id&#xe9;es" ID="ID_500089288" CREATED="1325629250546" MODIFIED="1325629255182"/>
+<node TEXT="si y&apos;a des armes &#xe0; feu, &#xe7;a peut &#xea;tre" ID="ID_1383101752" CREATED="1325629255433" MODIFIED="1325629263367">
+<node TEXT="une arme = culasse(chargeur)+canon+syst&#xe8;me de chargement+mire" ID="ID_70070450" CREATED="1325629263674" MODIFIED="1325629331676"/>
+<node TEXT="une munition = douille+poudre+balle(s)" ID="ID_314922727" CREATED="1325629332134" MODIFIED="1325629353167"/>
+</node>
+<node TEXT="on pourrait faire un peu aussi comme dans Dead Rising 2" ID="ID_309857456" CREATED="1325630324226" MODIFIED="1325630338688">
+<node TEXT="pour rappel, le mec voit des affiches de films d&apos;horreur barr&#xe9;s, et reproduit les atrocit&#xe9;s pour killer les zz" ID="ID_1190536762" CREATED="1325630339026" MODIFIED="1325630370791"/>
+<node TEXT="donc c&apos;est du craft" ID="ID_1531921143" CREATED="1325630371050" MODIFIED="1325630375647"/>
+<node TEXT="bien &quot;script&#xe9;&quot;" ID="ID_1060213393" CREATED="1325630376186" MODIFIED="1325630380843"/>
+<node TEXT="mais on se retrouve &#xe0; mettre des clous sur une bouteille de gaz pour faire une bombe" ID="ID_1569782626" CREATED="1325630381061" MODIFIED="1325630399544"/>
+<node TEXT="ou des tron&#xe7;onneuses au bout d&apos;une pagaie." ID="ID_1932749492" CREATED="1325630399794" MODIFIED="1325630412319"/>
+</node>
+<node TEXT="mais par exemple" ID="ID_1414636278" CREATED="1325630418347" MODIFIED="1325630424039">
+<node TEXT="un morceau de bois : son niveau de dmg d&#xe9;pend de son poids, de sa longueur, de m&#xea;me la maniabilit&#xe9; (moteur physique)" ID="ID_129219355" CREATED="1325630424330" MODIFIED="1325630461553"/>
+<node TEXT="on pourrait mettre un morceau de m&#xe9;tal pour faire une lance" ID="ID_679715652" CREATED="1325630461801" MODIFIED="1325630472799"/>
+<node TEXT="choisir comment s&apos;en servir (tranch&#xe9;, coup d&apos;estoc, ...)" ID="ID_827488666" CREATED="1325630473050" MODIFIED="1325630517922"/>
+</node>
+<node TEXT="probl&#xe8;me" ID="ID_1912466583" CREATED="1325629361898" MODIFIED="1325629366414">
+<node TEXT="trop de libert&#xe9;s = craft n&apos;impe dans ts les sens" ID="ID_807675903" CREATED="1325629367297" MODIFIED="1325629378551"/>
+<node TEXT="trop de recettes = craft frustrant car automatisable" ID="ID_665482978" CREATED="1325629378793" MODIFIED="1325629392623"/>
+</node>
+<node TEXT="faudrait pouvoir cr&#xe9;er des recettes au fur et &#xe0; mesure" ID="ID_420166914" CREATED="1325629398018" MODIFIED="1325629412199"/>
+<node TEXT="exemple du ciment : heu, &#xe7;a, c&apos;est typiquement une recette. Mais on doit pouvoir introduire de l&apos;al&#xe9;atoire" ID="ID_441812163" CREATED="1325630814969" MODIFIED="1325630841223"/>
+<node TEXT="exemple de l&apos;&#xe9;lectricit&#xe9; : heu, &#xe7;a aussi &#xe7;a sera de la recette. Mais &#xe7;a para&#xee;t essentiel" ID="ID_504710798" CREATED="1325630842154" MODIFIED="1325630866711"/>
+<node TEXT="(lanceur magn&#xe9;tique, lac &#xe9;lectrifi&#xe9;)" ID="ID_1443265388" CREATED="1325630906874" MODIFIED="1325630925183"/>
+</node>
+</node>
+</map>
